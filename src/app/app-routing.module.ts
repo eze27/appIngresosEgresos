3ea1cb9cdcff97c1bb0335352c76a55bd8c9b715
Eze <ezedev1994@gmail.com>
2019-06-18 10:29:33 -0500

    template con bootstrap & configuracion de rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { DasboardComponent } from "./dasboard/dasboard.component";
+import { DashboardRoutes } from "./dasboard/dashboard.routes";
+// 
+const routes:Routes = [ 
+     { path:'login',   component:LoginComponent },
+    { path:'register',   component:RegisterComponent },
+    { 
+        path:'',  
+        component:DasboardComponent ,
+        children:DashboardRoutes
+    },
+    { path:'**',   component:DasboardComponent }
+]
+
+@NgModule({
+imports: [ 
+    RouterModule.forRoot ( routes)
+],
+exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
